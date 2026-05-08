f36ad9e4bd8d6010992570a9583faa0f209171ac
jakobiusteubertius <96307320+jakobiusteubertius@users.noreply.github.com>
2026-05-08 23:13:41 +0200

    Include emulator in Android env helper

--- scripts/android-env.sh
+++ scripts/android-env.sh
@@ -2,4 +2,4 @@
 export JAVA_HOME="$HOME/Library/Java/JavaVirtualMachines/jdk-21.0.11+10/Contents/Home"
 export ANDROID_HOME="$HOME/Library/Android/sdk"
 export ANDROID_SDK_ROOT="$ANDROID_HOME"
-export PATH="$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools:$JAVA_HOME/bin:$PATH"
+export PATH="$ANDROID_HOME/emulator:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools:$JAVA_HOME/bin:$PATH"
